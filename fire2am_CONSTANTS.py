4d2b6a4ad83dc5766a51f0c7e4fcd871356df19c
fdobad <8339628-fdobad@users.noreply.gitlab.com>
2023-07-11 16:55:48 -0400

    CONSTANTS reformatted

--- fire2am_CONSTANTS.py
+++ fire2am_CONSTANTS.py
@@ -8,4 +8,3 @@ STATS_BASE_NAMES = ["Name", "Units"] + STATS_DESCRIBE_NAMES
 STATS_BASE_DF = DataFrame(STATS_BASE_NAMES, index=STATS_BASE_NAMES, columns=["Attributes"])
 GRID_NAMES = ["nsim", "ngrid", "burned"] + STATS_DESCRIBE_NAMES
 GRID_EMPTY_DF = DataFrame(columns=GRID_NAMES)
-
